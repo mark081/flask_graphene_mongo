3f6f17e4b56b4f52607868d1c525983b23e6a08b
Mark Cooper <cooper.canada@gmail.com>
2020-05-04 11:19:18 -0700

    added Lambda/API Gateway deployment scripts

--- redeploy.sh
+++ redeploy.sh
@@ -9,15 +9,8 @@
 
 export VIRTUAL_ENVS=$HOME/.virtualenvs
 
-virtualenv $VIRTUAL_ENVS/`pwd | sed 's#.*/##'`/.env
 source $VIRTUAL_ENVS/`pwd | sed 's#.*/##'`/.env/bin/activate
 
-pip install -r requirements.txt
-zappa init
-zappa deploy dev
+zappa redeploy dev
 
-
-echo " To undeploy: undeploy.sh"
-echo " To redeploy: redeploy.sh"
-echo ""
 echo " Zappa repo: https://github.com/Miserlou/Zappa"
--- undeploy.sh
+++ undeploy.sh
@@ -9,15 +9,10 @@
 
 export VIRTUAL_ENVS=$HOME/.virtualenvs
 
-virtualenv $VIRTUAL_ENVS/`pwd | sed 's#.*/##'`/.env
 source $VIRTUAL_ENVS/`pwd | sed 's#.*/##'`/.env/bin/activate
 
-pip install -r requirements.txt
-zappa init
-zappa deploy dev
+zappa undeploy dev
 
 
-echo " To undeploy: undeploy.sh"
-echo " To redeploy: redeploy.sh"
 echo ""
 echo " Zappa repo: https://github.com/Miserlou/Zappa"
